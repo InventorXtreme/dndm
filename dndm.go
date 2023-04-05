29de4c88425070896c03867bb2c9eb4d13c90555
InventorXtreme <inventorxtreme@gmail.com>
2023-04-05 13:27:51 -0400

    Added saving and loading the vfs

--- dndm.go
+++ dndm.go
@@ -5,10 +5,10 @@ import "github.com/chzyer/readline"
 import "regexp"
 import "strings"
 import "encoding/json"
-
+import "io/ioutil"
 type mapobj struct {
-    Val string
-    Sub map[string]*mapobj
+	Val string
+	Sub map[string]*mapobj
 }
 
 type completeholder struct {
@@ -16,50 +16,50 @@ type completeholder struct {
 }
 
 func SToMap(s string,m mapobj) *mapobj {
-    r := regexp.MustCompile("^[a-zA-Z0-9]*/")
-    a := r.FindString(s)
-    path := strings.TrimSuffix(a, "/")
-    newa := strings.TrimPrefix(s,a)
-    //fmt.Println(path)
-    if(len(newa) > 0){
-        _,ok := m.Sub[path]
-        if(!ok) {
-            return MakeMapObj("Item not found")
-        }
-        return SToMap(newa, *m.Sub[path])
-    } else {
-	_,ok := m.Sub[path]
-        if(!ok){
-            return MakeMapObj("Item not found")
-        }
-        return m.Sub[path]
-    }
+	r := regexp.MustCompile("^[a-zA-Z0-9]*/")
+	a := r.FindString(s)
+	path := strings.TrimSuffix(a, "/")
+	newa := strings.TrimPrefix(s,a)
+	//fmt.Println(path)
+	if(len(newa) > 0){
+		_,ok := m.Sub[path]
+		if(!ok) {
+            		return MakeMapObj("Item not found")
+        		}
+        	return SToMap(newa, *m.Sub[path])
+	} else {
+		_,ok := m.Sub[path]
+        	if(!ok){
+            		return MakeMapObj("Item not found")
+        	}
+		return m.Sub[path]
+	}
 }
 
 func MakeMapObj(x string) *mapobj {
-    ipm := new(mapobj)
-    ipm.Val = x
-    ipm.Sub = make(map[string]*mapobj)
-    return ipm
+	ipm := new(mapobj)
+	ipm.Val = x
+	ipm.Sub = make(map[string]*mapobj)
+	return ipm
 }
 
 
 func SetValToPath(s string, val string, m mapobj) *mapobj {
 	r := regexp.MustCompile("^[a-zA-Z0-9]*/")
-    a := r.FindString(s)
-    path := strings.TrimSuffix(a, "/")
-    newa := strings.TrimPrefix(s,a)
-    //fmt.Println(path)
-    if(len(newa) > 0){
-        _, ok := m.Sub[path]
-		if (!ok) {
-			m.Sub[path] = MakeMapObj("sub")
-		}
-        return SetValToPath(newa, val, *m.Sub[path])
-    } else {
+	a := r.FindString(s)
+	path := strings.TrimSuffix(a, "/")
+	newa := strings.TrimPrefix(s,a)
+	//fmt.Println(path)
+	if(len(newa) > 0){
+		_, ok := m.Sub[path]
+			if (!ok) {
+				m.Sub[path] = MakeMapObj("sub")
+			}
+		return SetValToPath(newa, val, *m.Sub[path])
+	} else {
 		m.Sub[path] = MakeMapObj(val)
 		//m.Val = val
-        return &m
+        	return &m
     } 
 }
 
@@ -70,6 +70,17 @@ func PathFormat(toformat string) string {
 	} else {
 		return e
 	}
+}
+
+func SaveMapObjToFile(name string, m *mapobj) {
+	jsonstring, _ := json.Marshal(m)
+	_ = ioutil.WriteFile(name, jsonstring, 0644)
+	
+}
+
+func ReadMapObjFromFile(name string, m *mapobj) {
+	filecontent, _ := ioutil.ReadFile(name)
+	_ = json.Unmarshal(filecontent,m)
 }
 
 func ListAllPaths(prefix string,m *mapobj) []string {
@@ -91,49 +102,62 @@ func (e completeholder ) compthing(a string) []string {
 }
 
 func main(){
-    maintime := MakeMapObj("sub")
-    maintime.Sub["players"] = MakeMapObj("sub")
-    SetValToPath("home/","test",*maintime)
+	maintime := MakeMapObj("sub")
+	maintime.Sub["players"] = MakeMapObj("sub")
+	SetValToPath("home/","test",*maintime)
 	fmt.Println(SToMap("home/", *maintime).Val)
-    a := completeholder{maintime}
-    pc := readline.NewPrefixCompleter(readline.PcItem("set",readline.PcItemDynamic(a.compthing)),readline.PcItem("get",readline.PcItemDynamic(a.compthing)),readline.PcItem("quit"))
+	a := completeholder{maintime}
+	pc := readline.NewPrefixCompleter(readline.PcItem("set",readline.PcItemDynamic(a.compthing)),readline.PcItem("get",readline.PcItemDynamic(a.compthing)),readline.PcItem("quit"))
 	fmt.Println(ListAllPaths("",maintime))
-    l, _ := readline.NewEx(&readline.Config{Prompt: "? ", AutoComplete: pc})
+	l, _ := readline.NewEx(&readline.Config{Prompt: "? ", AutoComplete: pc})
    
 	for true {
 		command, _ := l.Readline()
 		smand := strings.Split(command, " ")
 		if smand[0] == "quit" || smand[0] == "quit" {
-		    break
+			break
 		}
            
 		if smand[0] == "get" {
-		    fmt.Println(SToMap(PathFormat(command[4:]),*maintime).Val)
+			if len(smand) > 1 && len(smand[1]) > 0 {
+				fmt.Println(SToMap(PathFormat(smand[1]),*maintime).Val)
+			} else {
+				fmt.Println("ERR MUST SUPPLY PATH")
+			}
 		}
                 if smand[0] == "set" {
-		    
-		    SetValToPath(PathFormat(smand[1]),strings.Join(smand[2:]," "),*maintime)
+			if len(smand) > 1 && len(smand[1]) > 0 {
+				SetValToPath(PathFormat(smand[1]),strings.Join(smand[2:]," "),*maintime)
+			} else {
+				fmt.Println("ERR MUST SUPPLY PATH")
+			} 
+		}
+		if smand[0] == "save" {
+			fname := "default.json"
+			if len(smand) > 1 {
+				if len(smand[1]) > 0 {
+					fname = smand[1]
+				}
+			}
+			SaveMapObjToFile(fname,maintime)
+		}
+		if smand[0] == "load" {
+			fname := "default.json"
+			if len(smand) > 1 {
+				if len(smand[1]) > 0 {
+					fname = smand[1]
+				}
+			}
+			ReadMapObjFromFile(fname,maintime)
 		}
 	}
 		
 
 	
-    readline.SetAutoComplete(pc)
-    //b, _ := l.Readline()
+	readline.SetAutoComplete(pc)
+	//b, _ := l.Readline()
     
-    //fmt.Println(b)
+	//fmt.Println(b)
 
-    main := MakeMapObj("sub")
-    main.Sub["hehe"] = MakeMapObj("sub")
-    main.Sub["hehe"].Sub["test"] = MakeMapObj("data")
-    //fmt.Println(main.Sub["hehe"].Val)
-    fmt.Println(SToMap("hehe/",*main).Val)
-    b, err := json.Marshal(maintime)
-    fmt.Println(err)
-    var e mapobj
-    fmt.Println(maintime)
-    fmt.Println(string(b))
-    _ = json.Unmarshal(b, &e)
-    fmt.Println(e)
-    fmt.Println(e.Sub["home"])
+
 }
